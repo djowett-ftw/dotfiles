b2fa5d668ce5e0178d5e2f93642e4174a0a1b2cd
Jonas Baumann <jone@jone.ch>
2011-10-09 19:34:49 +0200

    emacs: move configs to local.el

---
+++ emacs.d/local.el
@@ -0,0 +1,26 @@
+(setq e-max-javascript-jslint-enabled t)
+(setq e-max-mac-use-shell-path t)
+
+(setq e-max-bundles '(
+                      accessibility
+                      completion
+                      css
+                      diff
+                      e-max-developer
+                      ergonomic
+                      git
+                      html
+                      javascript
+                      latex
+                      lisp
+                      markdown
+                      maximize
+                      org
+                      plone
+                      power-edit
+                      project
+                      python
+                      ruby
+                      terminal
+                      ))
+
