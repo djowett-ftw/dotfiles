b12e233cfd2cc6c615068639f06f6895d0948104
Jonas Baumann <jone@jone.ch>
2011-10-09 19:30:56 +0200

    emacs: configure plone instances

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(e-max-plone-known-buildout-instances (quote (("bin/instance" "fg") ("bin/instance1" "fg") ("bin/instanceadm" "fg") ("bin/serve" "") ("bin/feedcenter" "fg"))))
  '(e-max-project-location "/Users/jone/projects/")
  '(explicit-shell-file-name nil)
  '(term-scroll-show-maximum-output t))
