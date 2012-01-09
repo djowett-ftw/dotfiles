a19b5068878ab9c2527b656a51b1428fd1175632
Jonas Baumann <jone@jone.ch>
2012-01-09 12:41:05 +0100

    Emacs: set org files path.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(e-max-org-files "/Users/jone/idisk/notes/org")
  '(e-max-plone-known-buildout-instances (quote (("bin/instance" "fg") ("bin/instance1" "fg") ("bin/instanceadm" "fg") ("bin/serve" "") ("bin/feedcenter" "fg"))))
  '(e-max-project-location "/Users/jone/projects/")
  '(erc-autojoin-channels-alist (quote (("freenode.net" "#plone" "#PloneConf" "#Plone" "#rubyusergroupbern"))))
