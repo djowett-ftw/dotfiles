b3e86cccb3cfdae375bef025e4117e963d675a65
Jonas Baumann <jone@jone.ch>
2012-01-23 08:48:18 +0100

    Emacs: disable dabbrev case distinction.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(dabbrev-case-distinction nil)
+ '(dabbrev-case-fold-search nil)
  '(e-max-org-files "/Users/jone/idisk/notes/org")
  '(e-max-plone-known-buildout-instances (quote (("bin/instance" "fg") ("bin/instance1" "fg") ("bin/instanceadm" "fg") ("bin/serve" "") ("bin/feedcenter" "fg"))))
  '(e-max-project-location "/Users/jone/projects/")
