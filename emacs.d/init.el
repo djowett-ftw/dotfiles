c61afc388494f8e6b1c45b3068b485b358b35338
Jonas Baumann <jone@jone.ch>
2012-01-23 08:50:26 +0100

    Emacs: remove theme selection from init.el.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,6 +1,2 @@
 (setq e-max-repository (expand-file-name "/Users/jone/projects/e-max/"))
-
-;; see https://github.com/senny/theme-roller.el for a list of available themes
-(setq e-max-theme 'color-theme-lazy)
-
 (load (concat e-max-repository "e-max"))
