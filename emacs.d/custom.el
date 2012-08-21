cd26901a585524f0580c699d1bd6b696a5341257
Jonas Baumann <jone@jone.ch>
2012-08-21 13:17:05 +0200

    Emacs: update css / js indenting configuration.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(css-indent-offset 2)
  '(dabbrev-case-distinction nil)
  '(dabbrev-case-fold-search nil)
  '(e-max-javascript-jslint-enabled t)
@@ -24,9 +25,10 @@
  '(explicit-shell-file-name nil)
  '(indent-tabs-mode nil)
  '(ispell-extra-args (quote ("--lang=de")))
+ '(js-auto-indent-flag nil)
  '(js-curly-indent-offset 2)
  '(js-expr-indent-offset 2)
- '(js-indent-level 4)
+ '(js-indent-level 0)
  '(js-paren-indent-offset 2)
  '(js-square-indent-offset 2)
  '(magit-git-standard-options (quote ("--no-pager")))
