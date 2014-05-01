ca7eb33521db16e2858aeb416aacdad385547fdf
Jonas Baumann <jone@jone.ch>
2014-05-01 20:35:51 +0200

    Enable narrowing and disable minibuffer exit.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,2 +1,4 @@
 (setq cabbage-repository (expand-file-name "/Users/jone/projects/cabbage/"))
 (load (concat cabbage-repository "cabbage"))
+(put 'narrow-to-region 'disabled nil)
+(put 'ido-exit-minibuffer 'disabled nil)
