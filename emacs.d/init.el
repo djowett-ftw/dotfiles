57c16d98a8e70786447092f9f54602b0bde7e756
Jonas Baumann <jone@jone.ch>
2017-07-18 10:10:14 +0200

    set encoding environment vars in emacs.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,6 @@
+(setenv "LC_ALL" "en_US.UTF-8")
+(setenv "LANG" "en_US.UTF-8")
+
 (setq cabbage-repository (expand-file-name "/Users/jone/projects/cabbage/"))
 (load (concat cabbage-repository "cabbage"))
 (put 'narrow-to-region 'disabled nil)
