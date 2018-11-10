4e6ce8266a1cc51c3d16955ba74ec19139ad7f8c
Jonas Baumann <jone@jone.ch>
2018-11-10 21:54:20 +0100

    Remove old cabbage based emacs config.

--- emacs.d-cabbage/init.el
+++
@@ -1,9 +0,0 @@
-(setenv "LC_ALL" "en_US.UTF-8")
-(setenv "LANG" "en_US.UTF-8")
-
-(setq cabbage-repository (expand-file-name "/Users/jone/projects/cabbage/"))
-(load (concat cabbage-repository "cabbage"))
-(put 'narrow-to-region 'disabled nil)
-(put 'ido-exit-minibuffer 'disabled nil)
-(put 'upcase-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
