00f3cbb8ec13b3c993f86d85e4ac202f8d07e3f7
Jonas Baumann <jone@jone.ch>
2018-11-23 09:10:24 +0100

    zsh: function for cloning from our github.

--- zsh/plugins/jone/jone.plugin.zsh
+++ zsh/plugins/jone/jone.plugin.zsh
@@ -7,5 +7,10 @@ pwdcp() {
     echo -n `pwd | sed 's/ /\\\\ /g'`/$1 | pbcopy
 }
 
+g4c() {
+    set -x
+    git clone git@github.com:4teamwork/$@
+}
+
 . $ZSH_CUSTOM/plugins/jone/lazy-nvm.zsh
 . $ZSH_CUSTOM/plugins/jone/rdot.zsh
