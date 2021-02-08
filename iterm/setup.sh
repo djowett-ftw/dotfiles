e0e26c2c802121fea34620a88c20437a253ec11c
Jonas Baumann <jone@jone.ch>
2021-02-08 13:57:58 +0100

    Add iterm appearance switch script.

---
+++ iterm/setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+mkdir -p ~/Library/Application\ Support/iTerm2/Scripts/AutoLaunch
+ln -s switch_automatic.py ~/Library/Application\ Support/iTerm2/Scripts/AutoLaunch
+echo "Enable script in Scripts->AutoLunch menu in iTerm"
